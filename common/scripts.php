631c13d7354f6ed4db43a38296e162e0e6db32e5
Ronaldo Caetano <ronaldojr20@hotmail.com>
2019-05-12 00:28:25 -0300

    ajustes

--- common/scripts.php
+++ common/scripts.php
@@ -1,7 +1,3 @@
 <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
 <script src="<?php echo SITEBASE; ?>bootstrap/js/bootstrap.min.js"></script>
-<<<<<<< HEAD
 <script src="<?php echo SITEBASE; ?>assets/js/scripts.js"></script>
-=======
-<script src="./assets/js/scripts.js"></script>
->>>>>>> a92d7c24486f3a3261263ed016cc5658242f5c60
