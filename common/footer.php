5fedcb1c168e8a791f38c19efce8e96cb8aa41e0
cvt1501 <kevin_cavenatti@hotmail.com>
2019-06-09 22:17:53 -0300

    footer alteracoes

--- common/footer.php
+++ common/footer.php
@@ -2,9 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="texto col-md-10">
-                    <p>Trabalho realizado pelo grupo: Kevin Cavenatti Bueno, Lucas Ferraz de Lima, Lucas Lima de Faria Pacheco, Rodrigo Gianini do Prado Rodrigues, Thais Porto Mendonça.</p>
-                    <p>Com a orientação da professora Cristina Becker Navarro.</p>
-                    <p>Fatec Bragança Paulista - Curso de Gestão em Tecnologia da Informação. 2018-2019</p>
+                    <p>Fatec Bragança Paulista - Curso de Gestão em Tecnologia da Informação <br>2018-2019</p>
                 </div>
                 <div class="brasao col-md-2">
                     <img src="<?php echo SITEBASE; ?>assets/img/logo-fatec.png" />
