d1a0a88b2b394d9d26196cbee83e5c87fbc5a6e7
cvt1501 <kevin_cavenatti@hotmail.com>
2019-06-09 22:19:10 -0300

    footer alteracoes

--- common/footer.php
+++ common/footer.php
@@ -1,11 +1,8 @@
 <footer>
         <div class="container">
             <div class="row">
-                <div class="texto col-md-10">
+                <div class="texto col-md-12">
                     <p>Fatec Bragança Paulista - Curso de Gestão em Tecnologia da Informação <br>2018-2019</p>
-                </div>
-                <div class="brasao col-md-2">
-                    <img src="<?php echo SITEBASE; ?>assets/img/logo-fatec.png" />
                 </div>
             </div>
         </div>
